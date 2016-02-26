ec2d0d50bddace088c22471a732af6f57fdbdffb
Hamilton Greene <hamilton.greene@gmail.com>
2016-02-25 22:05:37 -0500

    A Tree testing in immutable_spec.js

--- test/immutable_spec.js
+++ test/immutable_spec.js
@@ -1,5 +1,5 @@
 import {expect} from 'chai';
-import {List} from 'immutable';
+import {List, Map} from 'immutable';
 
 describe('immutability', () => {
 
@@ -39,7 +39,39 @@ describe('immutability', () => {
 				'28 Days Later'
 			));
 		});
-		
+
+	});
+
+	describe('a tree', () => {
+
+		function addMovie(currentState, movie) {
+			return currentState.set(
+				'movies',
+				currentState.get('movies').push(movie)
+			);
+		}
+
+		it('is immutable', () => {
+			let state = Map({
+				movies: List.of('Trainspotting', '28 Days Later')
+			});
+			let nextState = addMovie(state, 'Sunshine');
+
+			expect(nextState).to.equal(Map({
+				movies: List.of(
+					'Trainspotting',
+					'28 Days Later',
+					'Sunshine'
+				)
+			}));
+			expect(state).to.equal(Map({
+				movies: List.of(
+					'Trainspotting',
+					'28 Days Later'
+				)
+			}));
+		});
+
 	});
 
 });
